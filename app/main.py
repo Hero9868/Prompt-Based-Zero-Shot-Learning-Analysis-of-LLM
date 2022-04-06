9cb39fba7451c6152e93844ab457179046307369
Pratyush Khuntia <29997337+Hero9868@users.noreply.github.com>
2022-04-06 13:42:48 +0530

    Updated Dashboard links

--- app/main.py
+++ app/main.py
@@ -7,20 +7,15 @@ st.set_page_config(
     page_title="Prompt Based Zero Shot Learning Analysis of Large Language Models",
     page_icon="📚",
     initial_sidebar_state="expanded",
-     menu_items={
-         'Get Help': 'https://www.extremelycoolapp.com/help',
-         'Report a bug': "https://www.extremelycoolapp.com/bug",
-         'About': "# This is a header. This is an *extremely* cool app!"
-     }
 )
 
 st.title("Prompt Based Zero Shot Learning Analysis of Large Language Models")
 st.subheader("About the Project")
 'We created a user-friendly developer platform and provide api for the same to show how prompt based learning, which is a zero shot learning approach for large language models, can optimize for trivial tasks and find an appropriate response given a suitable prompt. This will help us automate trivial generation tasks which normally need human input in them.'
 st.markdown("""
-- Github Repository for all the code can be found [here](github link here)
+- Github Repository for all the code can be found [here](https://github.com/Hero9868/Prompt-Based-Zero-Shot-Learning-Analysis-of-LLM)
 - Know more about the project with this [presentation](GDrive Link here)
-- Project proposal can be found [here](Google Doc Link Here)
+- Project proposal can be found [here](https://docs.google.com/document/d/1rSNhyRWpjSmNEXDzQejMFar8P2SgH4fe07EfejVUBbk/edit)
 
 ---
 
